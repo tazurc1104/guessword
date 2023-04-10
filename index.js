8c31ff164e9877648c62a94693dd7503521bae6d
tazurc1104 <127004476+tazurc1104@users.noreply.github.com>
2023-04-10 19:16:21 +0800

    Update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const letterButtons = document.querySelectorAll(".letters-buttons");
 const boxes = document.querySelectorAll(".boxes");
 const goButton = document.querySelector(".go-button");
-const eraseButton = document.querySelector(".erase-button");
 const header = document.querySelector(".page-header");
+
 const words = [
   "GRAPE",
   "MELON",
@@ -19,7 +18,6 @@ const words = [
   "BLACK",
   "BLAME",
   "BLIND",
-  "BLOOD",
   "COVER",
   "CHINA",
   "BROKE",
@@ -51,58 +49,39 @@ let endingIndex = 5;
 var guess = "";
 let startingBox = 0;
 let correct = 0;
-letterButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    if (startingIndex < endingIndex) {
-      boxes[startingIndex].textContent = button.textContent;
-      startingIndex++;
-    }
-  });
-});
 
-eraseButton.addEventListener("click", function() {
-  if (endingIndex - startingIndex <= 4){
-    boxes[startingIndex-1].textContent = '';
-    startingIndex--;
-  }
-  
-});
 
 
 
 goButton.addEventListener("click", function(){
-  if (startingIndex === endingIndex){
-    for (var i = guessLetters; guessLetters < endingIndex; guessLetters++){
-      guess += boxes[guessLetters].innerHTML;
-    }
-    endingIndex += 5;
-    for (var i = 0; i < word.length; i++){
-      if (guess[i] === word[i]){
-        boxes[startingBox].style.backgroundColor = "green";
-        correct++;
-        if (correct === 5) {
+    const guess = document.getElementById('guess-input').value;
+    const letters = guess.split("");
+    if (letters.length === 5){
+        for (var i = startingIndex; i < endingIndex; i++){
+            boxes[startingBox].innerHTML = letters[i].toUpperCase();
+            if (boxes[startingBox].innerHTML === word[i]){
+              boxes[startingBox].style.backgroundColor = "green";
+              correct++;
+            }
+            else {
+              for (var j = 0; j < word.length; j++){
+                if (boxes[i].innerHTML === word[j] && i !== j){
+                  boxes[startingBox].style.backgroundColor = "yellow";
+                }
+              }
+            }
+            startingBox++;
+            
+        }
+        if (correct === 5){
           header.innerHTML = "You Won";
           setTimeout(function() {
             location.reload();
           }, 3000); // 3000 milliseconds = 3 seconds
         }
-      }
-      else {
-        for (var j = 0; j < word.length; j++){
-          if (guess[i] === word[j] && i !== j){
-            boxes[startingBox].style.backgroundColor = "yellow";
-          }
         }
-      }
+        correct = 0;
     
-      
-      
-      startingBox++;
-    }
-   
-  }
-  
-  correct = 0;
-  guess = ""
+    
 });
 
